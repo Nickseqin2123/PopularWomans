f295ee00d5b535972d49f9559c5af81902c8b3b4
Nickseqin2123 <bletigri@gmail.com>
2024-05-13 18:43:49 +0300

    fix migrates

--- women/models.py
+++ women/models.py
@@ -7,6 +7,25 @@ class PublishedManager(models.Manager):
         return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"cat_slug": self.slug})
+
+
+class TagPost(models.Model):
+    tag = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    
+    def __str__(self) -> str:
+        return self.tag
+
+
 class Women(models.Model):
     class Status(models.IntegerChoices):
         DRAFT = 0, 'Черновик'
@@ -18,8 +37,9 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
-
+    cat = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(TagPost, blank=True, related_name='tags')
+    
     objects = models.Manager()
     published = PublishedManager()
 
@@ -34,15 +54,3 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-
-    def __str__(self):
-        return self.name
-    
-    def get_absolute_url(self):
-        return reverse("category", kwargs={"cat_slug": self.slug})
-    
