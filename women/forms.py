b3588c9c4178171b32fe00918f94f57f3d10f3f3
Nikita <bletigri@gmail.com>
2024-05-26 22:26:31 +0300

    add new class views

--- women/forms.py
+++ women/forms.py
@@ -17,7 +17,7 @@ class AddPostForm(forms.ModelForm):
         labels = {
             'slug': 'URL'
         }
-
+        
 
 class UploadFileForm(forms.Form):
     file = forms.ImageField(label='Файл')
--- women/views.py
+++ women/views.py
@@ -1,7 +1,10 @@
+from typing import Any
 from django.http import HttpResponse, HttpResponseNotFound, HttpRequest
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import AddPostForm, UploadFileForm
 from .models import Women, Category, TagPost, UploadFiles
+from django.views import View
+from django.views.generic import ListView
 
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -11,17 +14,21 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 ]
 
 
-def index(request):
-    posts = Women.published.all().select_related('cat')
-
-    data = {
+class WomenHome(ListView):
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    
+    extra_context = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': posts,
+        'posts': Women.published.all().select_related('cat'),
         'cat_selected': 0,
     }
-    return render(request, 'women/index.html', context=data)
-
+    
+    def get_queryset(self):
+        return Women.published.all().select_related('cat')
+    
+    
 
 def about(request: HttpRequest):
     if request.POST:
@@ -52,23 +59,33 @@ def show_post(request, post_slug):
     return render(request, 'women/post.html', data)
 
 
-def addpage(request: HttpRequest):
-    if request.POST:
+class AddPage(View):
+    
+    def get(self, request):
+        form = AddPostForm()
+        
+        data = {
+            'menu': menu,
+            'title': 'Добавление статьи',
+            'form': form
+        }
+        
+        return render(request, 'women/addpage.html', context=data)
+    
+    def post(self, request):
         form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('home')
-    else:
-        form = AddPostForm()
-        
-    data = {
-        'menu': menu,
-        'title': 'Добавление статьи',
-        'form': form
-    }
-    
-    return render(request, 'women/addpage.html', context=data)
 
+        data = {
+            'menu': menu,
+            'title': 'Добавление статьи',
+            'form': form
+        }
+        
+        return render(request, 'women/addpage.html', context=data)
+    
 
 def contact(request):
     return HttpResponse("Обратная связь")
@@ -78,19 +95,22 @@ def login(request):
     return HttpResponse("Авторизация")
 
 
-def show_category(request, cat_slug):
-    category = get_object_or_404(Category, slug=cat_slug)
-    posts = Women.published.filter(cat_id=category.pk).select_related('cat')
+class WomenCategory(ListView):
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
     
-    data = {
-        'title': f'Рубрика: {category.name}',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': category.pk,
-    }
-    return render(request, 'women/index.html', context=data)
-
-
+    def get_queryset(self):
+        return Women.published.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        cat = context['posts'][0].cat
+        context['title'] = 'Категория - ' + cat.name
+        context['menu'] = menu
+        context['cat_selected'] = cat.pk
+        return context
+        
+        
 def show_tag_postlist(request, tag_slug):
     tag = get_object_or_404(TagPost, slug=tag_slug)
     posts = tag.tags.filter(is_published=Women.Status.PUBLISHED).select_related('cat')
