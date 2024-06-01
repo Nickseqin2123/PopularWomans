0f2e2b372e77e3c719f2c6499809d494b7d7c585
Nikita <bletigri@gmail.com>
2024-06-01 13:11:49 +0300

    more add

--- users/views.py
+++ users/views.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.views import LoginView
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from users.forms import ProfileUserForm, UserPasswordChangeForm
+from django.contrib.auth import get_user_model
+from django.views.generic import UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
 
 
 class LoginUser(LoginView):
@@ -8,3 +15,31 @@ class LoginUser(LoginView):
     extra_context = {
         'title': 'Авторизация'
     }
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {
+        'title': 'Регистрация'
+    }
+    success_url = reverse_lazy('users:login')
+
+
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name = 'users/profile.html'
+    extra_context = {'title': "Профиль пользователя"}
+ 
+    def get_success_url(self):
+        return reverse_lazy('users:profile')
+    
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class UserPasswordChange(PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    success_url = reverse_lazy("users:password_change_done")
+    template_name = "users/password_change_form.html"
